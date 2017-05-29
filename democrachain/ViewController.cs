568869774dd29edfc2cbc80e8bf3a94a1795273e
Edson Alcalá <edson_alcala@msn.com>
2017-05-29 17:54:28 +0100

    * democrachain.csproj: added sending vote message while waiting   response from server
    * LoadingOverlay.cs:
    * ViewController.cs:

--- democrachain/ViewController.cs
+++ democrachain/ViewController.cs
@@ -15,6 +15,7 @@ namespace democrachain
 {
 	public partial class ViewController : UIViewController
 	{
+		LoadingOverlay loadPop;
 		protected ViewController(IntPtr handle) : base(handle)
 		{
 			// Note: this .ctor should not contain any initialization logic		
@@ -33,11 +34,18 @@ namespace democrachain
 
 		public async Task<bool> DownloadHomepage()
 		{
+			var bounds = UIScreen.MainScreen.Bounds;
+
+			// show the loading overlay on the UI thread using the correct orientation sizing
+			loadPop = new LoadingOverlay(bounds); // using field from step 2
+			View.Add (loadPop);
+
 			var httpClient = new HttpClient(); // Xamarin supports HttpClient!
-			//httpClient.DefaultRequestHeaders.Accept.Clear();
-			//httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+											   //httpClient.DefaultRequestHeaders.Accept.Clear();
+											   //httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
 			HttpResponseMessage response = await httpClient.GetAsync("http://democrachain-api.azurewebsites.net/vote");
+			loadPop.Hide();
 			if (response.IsSuccessStatusCode)
 			{
 				return true;
@@ -46,7 +54,6 @@ namespace democrachain
 			{
 				return false;
 			}
-
 		}
 		public override bool ShouldAutorotateToInterfaceOrientation(UIInterfaceOrientation toInterfaceOrientation)
 		{
