b9ed71e2b1f0751d999b41b61401d18f37b93c6c
Edson Alcalá <edson_alcala@msn.com>
2017-05-31 10:52:51 +0100

    * Contents.json: added default page and navigation
    * Main.storyboard:
    * ViewController.cs:
    * TestController.cs:
    * democrachain.csproj:
    * TestController.designer.cs:
    * first-cube-05.png:
    * blockchain-01.png:
    * first-cube-01.png:

---
+++ democrachain/TestController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace democrachain
+{
+    public partial class TestController : UIViewController
+    {
+        public TestController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
--- democrachain/ViewController.cs
+++ democrachain/ViewController.cs
@@ -29,7 +29,31 @@ namespace democrachain
 			{
 				Task<bool> sizeTask = DownloadHomepage();
 				var intResult = await sizeTask;
+				//UIAlertView avAlert = new UIAlertView("Notification", alert, null, "OK", null);
+				//avAlert.Show();
+				var alert = UIAlertController.Create("Success", "Your vote has been sent!", UIAlertControllerStyle.Alert);
+				if (alert.PopoverPresentationController != null)
+					alert.PopoverPresentationController.BarButtonItem = sender as UIBarButtonItem;
+
+				alert.AddAction(UIAlertAction.Create("Ok", UIAlertActionStyle.Cancel, action => Ok()));
+				//alert.AddAction(UIAlertAction.Create("Snooze", UIAlertActionStyle.Default, action => Snooze()));
+
+				PresentViewController(alert, animated: true, completionHandler: null);
+
+				//navigate to the other page
+
 			};
+		}
+
+		//void Show
+		void Ok()
+		{
+			//throw new NotImplementedException();
+			TestController homePage = this.Storyboard.InstantiateViewController("TestController") as TestController;
+			if (homePage != null)
+			{
+				this.NavigationController.PushViewController(homePage, true);
+			}
 		}
 
 		public async Task<bool> DownloadHomepage()
@@ -38,7 +62,7 @@ namespace democrachain
 
 			// show the loading overlay on the UI thread using the correct orientation sizing
 			loadPop = new LoadingOverlay(bounds); // using field from step 2
-			View.Add (loadPop);
+			View.Add(loadPop);
 
 			var httpClient = new HttpClient(); // Xamarin supports HttpClient!
 											   //httpClient.DefaultRequestHeaders.Accept.Clear();
@@ -65,6 +89,12 @@ namespace democrachain
 		{
 			base.DidReceiveMemoryWarning();
 			// Release any cached data, images, etc that aren't in use.
+		}
+
+		public override void ViewWillAppear(bool animated)
+		{
+			base.ViewWillAppear(animated);
+			this.NavigationController.SetNavigationBarHidden(true, false);
 		}
 	}
 }
